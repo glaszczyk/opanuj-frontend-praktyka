f897a1b3456b4da52c55e14a6ddff9a083ccdd29
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2024-07-11 10:25:16 +0200

    OPF-02-rest-countries: Add settings, router and RestCountries page

---
+++ practice/vue-app/src/module2/rest-countries/views/RestCountries.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Rest Countries</h1>
+    <div>
+      <p>Here is REST Countries</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
