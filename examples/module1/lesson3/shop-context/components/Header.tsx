177c02ce77db76a86e325d24ee9402a4d043139c
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2024-08-16 11:01:49 +0200

    OPF-L03-context: Extract AddToCartButton and ItemCounter button

--- examples/module1/lesson3/shop-context/components/Header.tsx
+++ examples/module1/lesson3/shop-context/components/Header.tsx
@@ -1,15 +1,22 @@
-import { useContext } from 'react';
+import { Dispatch, SetStateAction, useContext } from 'react';
 import { BsBag } from 'react-icons/bs';
 import { CiShop } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../contexts/CartContext';
 
 interface HeaderProps {
-  setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsSidebarOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+function ItemCounter() {
+  const { itemAmount } = useContext(CartContext);
+  return <div
+    className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center">
+    {itemAmount}
+  </div>;
 }
 
 const Header = ({ setIsSidebarOpen }: HeaderProps) => {
-  const { itemAmount } = useContext(CartContext);
 
   return (
     <header className={`bg-none py-6 fixed w-full z-10 lg:px-8 transition-all`}>
@@ -22,9 +29,7 @@ const Header = ({ setIsSidebarOpen }: HeaderProps) => {
           className="cursor-pointer flex relative mr-8"
         >
           <BsBag className="text-2xl" />
-          <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center">
-            {itemAmount}
-          </div>
+          <ItemCounter />
         </div>
       </div>
     </header>
