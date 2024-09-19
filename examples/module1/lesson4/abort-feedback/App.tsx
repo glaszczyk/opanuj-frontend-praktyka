f47333f3d2fc95babef2d6debb6cd6a4e3bd888c
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2024-09-19 10:19:09 +0200

    OPF-L04-abort-feedback: Add abort controller. Reset fetch on button

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface User {
   id: number;
@@ -8,28 +8,58 @@ interface User {
 const API_URL = '/api/data/users?timeout=10000';
 
 const App = () => {
+  const [controller, setController] = useState<AbortController | null>(null);
   const [users, setUsers] = useState<User[]>([]);
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
 
-  useEffect(() => {
-    fetch(API_URL)
+  const fetchUsersData = (signal: AbortSignal) => {
+    fetch(API_URL, { signal })
       .then((res) => res.json())
       .then(({ users }) => {
         setUsers(users);
+        setIsButtonVisible(false);
       });
+  };
+
+  const withAbort = (fetchFn: Function) => {
+    withDelay(5000, showButton);
+    const newController = new AbortController();
+    setController(newController);
+    const signal = newController.signal;
+    fetchFn(signal);
+  }
+
+  const withDelay = (ms: number, callback: Function) => setTimeout(() => callback()
+    , ms);
+
+  const showButton = () => setIsButtonVisible(true);
+
+  const resetFetching = () => {
+    console.log('reset fetching');
+    if (controller) {
+      setIsButtonVisible(false);
+      controller.abort('Aborted by user');
+      withAbort(fetchUsersData);
+    }
+  };
+
+  useEffect(() => {
+    withAbort(fetchUsersData);
   }, []);
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between py-4">
         <h1 className="text-2xl font-bold">Users</h1>
-        <div className="flex flex-row items-center">
+        {isButtonVisible && <div className="flex flex-row items-center">
           <p className="mr-2">
             Sorry, there seems to be connectivity issues...
           </p>
-          <button className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
+          <button onClick={resetFetching}
+                  className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
             Try again
           </button>
-        </div>
+        </div>}
       </div>
       <ul className="space-y-2">
         {users.map((user, index) => (
